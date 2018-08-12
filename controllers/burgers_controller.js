dd9487e4b5fc9fbe43114eb81e5cdb736702a2d0
Kebo <36769833+Kebo83@users.noreply.github.com>
2018-08-11 23:23:16 -0700

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,48 +13,47 @@ router.get('/', function (req, res) {
     res.render("index", hbsObject);
   });
 });
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
+router.post("/burgers", function(req, res) {
+  console.log("this works");
+  burger.insertOne([
+    "burger_name"
+    // , "devoured"
   ], [
-    req.body.name, req.body
-    // .sleepy
+    req.body.burger_name
+    // , req.body.devoured
+
   ], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+   
+    res.redirect('/');
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var devoured = "id = " + req.params.id;
-
-  console.log("devoured", devoured);
-
-  burger.update({
-    burger_name: req.body
-    // .sleepy
-  }, devoured, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
+  console.log("this works");
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  // console.log("devoured", condition);
+
+    burger.updateOne({
+    devoured: true
+  }, condition, function(data) {
+    res.redirect('/');
   });
 });
+
+// router.delete("/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 
 module.exports = router;
